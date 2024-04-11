96a7ccf8cd4ca1fc439cd1450b42358d2855e1f0
Zerophymyr <Zzer0ph@gmail>
2024-04-11 07:04:02 +0200

    Revert "Revert "added endpoints""
    This reverts commit 738ae3e60813d1c9ce540a6c74e55963cbf7a771.

--- backend/Api/Services/IProductService.cs
+++ backend/Api/Services/IProductService.cs
@@ -4,7 +4,8 @@ namespace Api.Services;
 
 public interface IProductService
 {
-    Task<List<Product>> GetNumberOfLatestProductsByOffset(int offset, int number);
+    Task<List<Product>> GetNumberOfLatestProductsByOffset(int offset, int number, string category);
     Task<Product?> GetProductByID(int id);
     Task<Product> CreateProduct(string name, string description, int Price, string Condition, string category);
+    Task<List<string>> GetAllProductCategories();
 }
--- backend/Api/Services/ProductService.cs
+++ backend/Api/Services/ProductService.cs
@@ -6,21 +6,40 @@ namespace Api.Services;
 
 public class ProductService(AppDbContext context) : IProductService
 {
-    public async Task<List<Product>> GetNumberOfLatestProductsByOffset(int offset, int number)
+    public async Task<List<Product>> GetNumberOfLatestProductsByOffset(int offset, int number, string category)
     {
+        if (category.ToLower() != "news")
+        {
+            var prod = await context.Product
+                .Where(p => p.Category == category)
+                .Skip(offset) 
+                .Take(number) 
+                .ToListAsync();
+            return prod;
+        }
         var products = await context.Product
             .OrderBy(p => p.PostedAtDate) 
             .Skip(offset) 
             .Take(number) 
-            .ToListAsync(); 
-
+            .ToListAsync();
         return products;
     }
 
-    public Task<Product?> GetProductByID(int id)
+    public async Task<Product?> GetProductByID(int id)
     {
-        throw new NotImplementedException();
+    return await context.Product.FindAsync(id);
     }
+    
+    public async Task<List<string>> GetAllProductCategories()
+    {
+        var categories = await context.Product
+            .Select(p => p.Category)
+            .Distinct()
+            .ToListAsync();
+        categories.Add("News");
+        return categories;
+    }
+
 
     public async Task<Product> CreateProduct(string name, string description, int price, string condition,string category)
     {
