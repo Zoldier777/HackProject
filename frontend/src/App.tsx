9879aaac3c66245bfaf735c46ddbb38acba38e66
Zerophymyr <Zzer0ph@gmail>
2024-04-11 07:34:14 +0200

    revert mistake, fixed back routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,28 @@
 
 import Footer from './Footer'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "react-query"
 import ProductGallery from './productgalley/Productgallery'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './Components/navbar/Navbar'
 
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-          <ProductGallery/>
-      </QueryClientProvider>
-      <Footer/>
-    </>
+
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />} >
+            <Route path="/" element={<ProductGallery/>} /> 
+            <Route path="/search" element={<ProductGallery/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+
+
   )
 }
 
--- frontend/src/productgalley/Productgallery.tsx
+++ frontend/src/productgalley/Productgallery.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from "react-query"
 import Product from "../product/Product";
 
 const ProductGallery = () => {
     const [pageOffset, setPageOffset] = useState<number>(0);
     const [searchParams] = useSearchParams();
 
-    async function getItems(query_page: number, query_search: string): Promise<product[]> {
-        const url = `http://localhost:5180/api/Product?offset=${query_page}&number=${6}`;
+    async function getItems(query_page: number,): Promise<product[]> {
+        const url = `http://localhost:5180/api/Product?offset=${query_page}&number=${6}&category=News`;
         const response = await fetch(url);
         const json_response = await response.json();
         const products = json_response as product[];
@@ -21,7 +21,8 @@ const ProductGallery = () => {
         data: products,
     } = useQuery<product[], Error>({
         queryKey: ['todos', pageOffset],
-        queryFn: () => getItems(pageOffset, searchParams.get("search") ?? "news")
+        queryFn: () => getItems(pageOffset),
+        keepPreviousData: true
     });
 
     if (isLoading) return 'Loading...';
@@ -33,19 +34,12 @@ const ProductGallery = () => {
             <h1 className="text-4xl font-bold text-dark py-4 ">Category</h1>
             <div className="grid grid-cols-3 gap-10 pb-10">
                 {products!.map(product => (
-                    <Product key={product.id}   
-                    id={product.id}
-                    name={product.name}
-                    price={product.price}
-                    description={product.description}
-                    condition={product.condition}
-                    category={product.category}
-                    postedAtDate={product.postedAtDate}/>
+                    <Product key={product.id} {...product} />
                 ))}
             </div>
             <div className="flex justify-center gap-10 w-full pb-4" >
-                <button className="btn" onClick={() => setPageOffset((page) => Math.max(0, page - 5))}>Previous</button>
-                <button className="btn" onClick={() => setPageOffset((page) => Math.max(0, page + 5))}>Next</button>
+                <button className="btn" onClick={() => setPageOffset((page) => Math.max(0, page - 6))}>Previous</button>
+                <button className="btn" onClick={() => setPageOffset((page) => Math.max(0, page + 6))}>Next</button>
             </div>
         </div>
     );
